798731e452badfcd0919480624cefe958c636bf7
MatysZeZ <casyneu@gmail.com>
2022-08-07 22:55:16 +0000

    Readings for fabric tiquet

--- generate_readme.sh
+++ generate_readme.sh
@@ -6,13 +6,14 @@ do
     do
         for deploy in multi single split
         do
-            echo "# READINGS 1-10 $tool $app $deploy" > /$tool/$app/$deploy/README.md
-            echo "\`\`\`" >> /$tool/$app/$deploy/README.md
+            echo "# READINGS 1-10 $tool $app $deploy" > ./$tool/$app/$deploy/README.md
+	    echo "" >> ./$tool/$app/$deploy/README.md
+            echo "\`\`\`" >> ./$tool/$app/$deploy/README.md
             for i in {1..10}
             do
-                cat /$tool/$app/$deploy/$i/deploy_time.txt >> /$tool/$app/$deploy/README.md
+                cat ./$tool/$app/$deploy/$i/deploy_time.txt >> ./$tool/$app/$deploy/README.md
             done
-            echo "\`\`\`" >> /$tool/$app/$deploy/README.md
+            echo "\`\`\`" >> ./$tool/$app/$deploy/README.md
         done
         echo "Done $tool $app $deploy"
     done

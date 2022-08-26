ce716597e9bf80cc385f3b22f4b06f8a78694310
MatysZeZ <casyneu@gmail.com>
2022-08-26 16:00:23 +0000

    Zero attempt test fin

--- error.sh
+++ error.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-for tool in ansible bash fabric
-do
-    for app in grafana static tiquet
-    do
-        for deploy in multi single split
-        do
-                ls -la ./$tool/$app/$deploy/1/net-* >> error.txt
-                echo "$tool $app $deploy" >> error.txt
-
-	    done
+for tool in ansible bash fabric; do
+    for app in grafana tiquet; do
+        for deploy in multi single split; do
+            ls -la ./$tool/$app/$deploy/0/net-* >>error.txt
+            ls -la ./$tool/$app/$deploy/0/ps-* >>error.txt
+            echo "$tool $app $deploy" >>error.txt
+            echo "\n" >>error.txt
+        done
     done
 done
-	
+
+for tool in ansible bash fabric; do
+    app="static"
+    for deploy in multi single; do
+        ls -la ./$tool/$app/$deploy/0/net-* >>error.txt
+        ls -la ./$tool/$app/$deploy/0/ps-* >>error.txt
+        echo "$tool $app $deploy" >>error.txt
+        echo "\n" >>error.txt
+    done
+done
